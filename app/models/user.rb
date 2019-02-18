23cda1a49baffaffa6bc743d67d5f927b6df00c8
Naudria <rima.jean@gmail.com>
2019-02-18 09:50:31 -0600

    Add User associations; a User has_many Ratings, and many Books, through Ratings; many-to-many relationships between Users and Books

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :ratings
+  has_many :books, through: :ratings
+
+  has_secure_password
+  # A method that uses Bcrypt to hash passwords
+  validates_presence_of :name, :email
+  validates_uniqueness_of :name, :email
 end
