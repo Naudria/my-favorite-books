5503c537f17468cfb95249229a941f07bfe1c5dd
Naudria <rima.jean@gmail.com>
2019-02-18 13:06:49 -0600

    Create methods in books controller to GET index of books, POST a new book via user submitted form, with strong params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,47 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def new
+    @book = Book.new
   end
 
   def show
+    @book = Book.find_by(id: params[:id])
+  end
+
+  def create
+    @book = Book.find_or_create_by(book_params)
+    @book.creator_id = current_user.id
+    if @book.valid?
+      # Run the validations WITHOUT attempting to save to the database,
+      # returning true if valid, false if not.
+      @book.save
+      redirect_to @book
+      # Tells browser to perform a NEW REQUEST (throw @book away and let
+      # the show action worry about re-reading it from the database)
+    else
+      render :new
+      #If book is invalid, hold on to @book (because of useful error messages)
+      # and re-render the :new page.
+    end
   end
 
   def edit
+    @book = Book.find_by(id: params[:id])
   end
+
+  def update
+    @book = Book.find_by(id: params[:id])
+    @book.update(book_params)
+    redirect_to @book
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :genre, :author)
+  end
+
 end
