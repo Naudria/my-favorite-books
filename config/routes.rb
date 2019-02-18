12e3ff73ba65d19cd3ed32b486d0d8642358ebc5
Naudria <rima.jean@gmail.com>
2019-02-18 10:00:03 -0600

    Generate users controller and configure router to create new, create, and show routes mapped to users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
   root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:new, :create, :show]
 end
