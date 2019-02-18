747ef2df83d7cfa3038aaf4fb755251aea08c1b1
Naudria <rima.jean@gmail.com>
2019-02-18 11:42:59 -0600

    Add 3 methods to sessions controller: new, create, destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :current_user
+  protect_from_forgery with: :exception
 
 
   def current_user
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,28 @@
 class SessionsController < ApplicationController
+
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+
+      session[:user_id] = @user.id
+
+      redirect_to @user
+    else
+      flash[:message] = "Your username or password were incorrect."
+      redirect_to signin_path
+
+    end
+  end
+end
+
+def destroy
+  session.delete :user_id
+
+  redirect_to_root_path
+end
+
 end
