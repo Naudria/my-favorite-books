aade6b75a00c72b721af9c445f25c79cba721385
Naudria <rima.jean@gmail.com>
2019-02-18 11:53:22 -0600

    Add has_secure_password to implement bcrypt gem and user_params in users controller for authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def show
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
 end
