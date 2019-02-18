aaf310248c3f796157593506afbc498c9a8b6f1b
Naudria <rima.jean@gmail.com>
2019-02-18 10:14:42 -0600

    Generate books controller and add associations to Book model: A Book has_many Ratings, and many Users, through Ratings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'books/index'
+  get 'books/new'
+  get 'books/show'
+  get 'books/edit'
   root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
