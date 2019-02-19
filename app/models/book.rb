6f10db8fda55047f78b1a22d112e64fa36e7035c
Naudria <rima.jean@gmail.com>
2019-02-19 10:34:50 -0600

    Create class methods on Book model that determine all books' average ratings, popularity ratings, and a method to sort books by popularity

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,26 @@ class Book < ApplicationRecord
 
   validates_presence_of :title, :author, :genre
   validates_uniqueness_of :title
+
+  def average_rating
+    Rating.where(book_id: self.id).average(:stars).to_i
+  end
+
+  def popularity_rating
+    self.average_rating + self.ratings.count
+  end
+
+  def popular?
+    if self.average_rating >= 3
+      "#{self.title}" + " " + "#{self.popularity_rating}"
+    end
+  end
+
+  def self.popularity
+
+    Book.all.sort_by do |book|
+      book.average_rating
+    end.reverse
+  end
+
 end
