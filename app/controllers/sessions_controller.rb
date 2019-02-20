d3ca588996eaf55b5451b7020b719a964091a7f0
Naudria <rima.jean@gmail.com>
2019-02-20 09:59:21 -0600

    Fix signin bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
 
       redirect_to @user
     else
-      @user = User.find_by(email: params[:user][:email])
+      @user = User.find_by(name: params[:user][:name])
       if @user && @user.authenticate(params[:user][:password])
 
         session[:user_id] = @user.id
